453c7edce51cd6c89d22273c4a778c16839ae51f
Francis Le Roy <francis.leroy@protonmail.ch>
2019-11-19 18:05:42 +0100

    File tree ok

---
+++ src/lexer_parser/mod.rs
@@ -0,0 +1,25 @@
+use crate::utils::error::ESError;
+
+pub enum LineType {
+    Rule,
+	Fact,
+	Query,
+}
+
+fn get_line(line: &mut String) -> Result<(), ESError>
+{
+	// Get the line without comment
+	Ok(())
+}
+
+fn get_line_type(line: &String) -> Result<LineType, ESError>
+{
+	//Get line type
+	Ok(LineType::Fact) //TODO CHANGE
+}
+
+pub fn process_file(path: &String) -> Result<(), ESError>
+{
+	let file = std::fs::File::open(path)?;
+	Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@
 /*   By: fle-roy <francis.leroy@protonmail.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 11:09:23 by gsmith            #+#    #+#             */
-/*   Updated: 2019/11/19 13:47:19 by fle-roy          ###   ########.fr       */
+/*   Updated: 2019/11/19 18:05:15 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-mod ES;
+pub mod lexer_parser;
+pub mod resolving;
+pub mod utils;
 
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
 	
-	ES::LP::process_file(&args[0]);
+	lexer_parser::process_file(&args[0]);
 }
