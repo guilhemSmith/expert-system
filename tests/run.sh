b8738aaf7cd4dc4252ec8e954323cc2c5c224fa8
Guilhem Smith <smith.guilhem@gmail.com>
2019-11-23 13:30:46 +0100

    Add the expected output in a separate folder.

--- tests/run.sh
+++ tests/run.sh
@@ -1,2 +1,8 @@
-files=`ls tests/samples`;
-echo $files;
+input_files=`ls tests/input-files`;
+input_dir="tests/input-files/";
+output_dir="tests/expected-output/";
+
+for fn in $input_files; do
+	result=`./target/debug/expert-system $input_dir$fn`;
+	echo $result;
+done
